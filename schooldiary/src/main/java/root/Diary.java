3f5ccac8612973e886ba4b5212746d1d44e2dca4
ghost <gladiator9000@inbox.ru>
2018-09-29 18:26:30 +0300

    начальное меню дневника

--- schooldiary/src/main/java/root/Diary.java
+++ schooldiary/src/main/java/root/Diary.java
@@ -1,4 +1,23 @@
 package root;
 
-public class Diary {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import menu.MenuDiary;
+
+public class Diary extends Application implements Root {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        MenuDiary menuDiary = new MenuDiary();
+        ROOT.getChildren().add(menuDiary.getMenuBar());
+
+        WINDOW.setScene(SCENE_WINDOW);
+        WINDOW.setTitle("Дневник");
+        WINDOW.show();
+    }
+
+    public static void main (String[] args) {
+        launch(args);
+    }
 }
